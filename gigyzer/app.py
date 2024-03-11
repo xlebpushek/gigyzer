a6df8343e363d3d3f94605ebe7bd763c795d1f25
xlebpushek <xlebpushek@gmail.com>
2024-03-11 17:33:55 +0300

    Refactor import statements in app.py

--- gigyzer/app.py
+++ gigyzer/app.py
@@ -2,7 +2,7 @@ from pyrogram import compose, filters
 from pyrogram.handlers import MessageHandler
 
 from gigyzer import botClient, userClient
-from gigyzer.handlers.commands import analyze_command, start_command
+from gigyzer.handlers import analyze_command, start_command
 
 botClient.add_handler(MessageHandler(
     start_command,
